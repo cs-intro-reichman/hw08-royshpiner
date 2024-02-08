a2fc57f93fafb3e63f02e010323d6f372daee7eb
Roy Shpiner <shpiner11@Roys-MacBook-Air.local>
2024-02-08 14:40:02 +0200

    second

--- Track.java
+++ Track.java
@@ -43,6 +43,12 @@ class Track {
     // Where seconds is always two digits. For example, "3:17" or "12:05".
     private String formattedDuration(int totalSeconds) {
         //// replace the following statement with your code
-        return "";
+        int minutes = totalSeconds/60;
+        int seconds = totalSeconds%60;
+        String sMinutes = Integer.toString(minutes);
+        String sSeconds = Integer.toString(seconds);
+
+
+        return sMinutes+":"+sSeconds;
     }
 }
